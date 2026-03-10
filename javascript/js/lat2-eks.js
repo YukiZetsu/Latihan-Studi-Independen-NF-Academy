74bb800a7e00283f5a52e8b11f1e582c1c45bd00
YukiZetsu <didik.pct001@gmail.com>
2026-03-10 16:11:25 +0700

    Upload Latihan JS

---
+++ javascript/js/lat2-eks.js
@@ -0,0 +1,3 @@
+alert("Selamat Datang di Website NFA");
+document.write("<h1>WEBSITE NURULFIKRI ACADEMY</h1>");  //output tercetak di isi web
+console.log("Alamat Pelatihan di NFA"); //output tercetak di console
